cfcfb98eaed61df4bd93971e38035a81cb2b050c
dodosaurus <kovac.jozef25@gmail.com>
2024-04-17 06:52:22 +0200

    new enemy, gun enhanced

---
+++ scripts/bee.gd
@@ -0,0 +1,5 @@
+extends Area2D
+
+func _on_area_entered(area):
+	print("bee was hit")
+	area.queue_free()
--- scripts/player.gd
+++ scripts/player.gd
@@ -4,6 +4,7 @@ extends CharacterBody2D
 var direction_x := 0.0
 @export var speed := 150
 @onready var cd_timer = $Timers/CooldownTimer
+@onready var fire_timer = $Timers/FireTimer
 
 var can_shoot := true
 var facing_right := true
@@ -11,7 +12,7 @@ var has_gun := false
 
 signal shoot(pos: Vector2, facing_right: bool)
 
-func _process(delta):
+func _process(_delta):
 	get_input()
 	apply_gravity()
 	get_facing_direction()
@@ -19,6 +20,7 @@ func _process(delta):
 	
 	velocity.x = direction_x
 	
+	#automatically using delta
 	move_and_slide()
 	
 func get_input():
@@ -27,10 +29,12 @@ func get_input():
 	if Input.is_action_pressed("jump") and is_on_floor():
 		velocity.y -= 200
 		
-	if Input.is_action_just_pressed("shoot") and can_shoot:
+	if Input.is_action_just_pressed("shoot") and can_shoot and has_gun:
 		can_shoot = false
 		print("shoot")
+		$Fire.get_child(facing_right).show()
 		shoot.emit(global_position, facing_right)
+		fire_timer.start()
 		cd_timer.start()
 	
 func apply_gravity():
@@ -55,3 +59,7 @@ func get_animation():
 	
 func _on_cooldown_timer_timeout():
 	can_shoot = true
+
+func _on_fire_timer_timeout():
+	for child in $Fire.get_children():
+		child.hide()
